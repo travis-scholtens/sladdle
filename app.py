8238fcb454436f70695a0dc1d79d776cccbcecc0
travis-scholtens <113060760+travis-scholtens@users.noreply.github.com>
2022-09-23 10:24:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -34,27 +34,43 @@ def parse_date(d):
   if d == 'tomorrow':
     return datetime.date.today() + datetime.timedelta(days=1)
   try:
-    return dateutil.parser.parse(d).date()
+    date = dateutil.parser.parse(d).date()
+    if date.month <= 6 and date.year == datetime.date.today().year:
+      return date + datetime.timedelta(years=1)
+    return date
   except dateutil.parser.ParserError:
     return None
 
 
+def create(channel, date):
+  if not date:
+    return 'Missing date'
+        
+  db.collection('lineups').collection(channel).document(str(date)).set({
+        'play_on_date': str(date),
+        'courts' = { i: (None, None) for i in range(1, 7)}
+    })
+  return f'New empty lineup for {channel} on {date}'
+
 @app.route("/lineup", methods=['POST'])
 @slack_sig_auth
 def lineup():
+    channel = request.form['channel_id']
     ts = request.form['text'].split()
-    maybe_date =
-    if not len(ts):
-      return(show())
-    if len(ts) == 1:
-      try:
-        return(show((ts[0])))
-      except dateutil.parser.ParserError:
-        return 'Expected date, like "/lineup tomorrow"'
-    db.collection(u'test').document(u'x').set({
-        u'test': request.form['text']
-    })
-    return 'hi <@travis.scholtens>'
+    date = None
+    (maybe_date, cmds) = (ts[0] if ts else None, ts[1:])
+    if maybe_date:
+      date = parse_date(maybe_date)
+      if not date:
+        cmds.insert(0, maybe_date)
+    if not cmds:
+      return(show(channel, date))
+    if cmds == ['new']:
+      return create(channel, date)
+    try:
+      return court(channel, date, int(cmds[0]), cmds[1:])
+    except ValueError:
+      return 'Expected court number (1-6)'
 
 
 if __name__ == "__main__":
