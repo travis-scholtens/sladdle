d5aa3feb08cdd1e7164a925fa87b72259080fa4e
travis-scholtens <113060760+travis-scholtens@users.noreply.github.com>
2022-09-23 09:53:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,37 @@ def event():
     return request.json['challenge']
 
 
+def parse_date(d):
+  try:
+    int(d)
+    return None
+  except ValueError:
+    pass
+  d = d.lower()
+  if d == 'yesterday':
+    return datetime.date.today() - datetime.timedelta(days=1)
+  if d == 'today':
+    return datetime.date.today()
+  if d == 'tomorrow':
+    return datetime.date.today() + datetime.timedelta(days=1)
+  try:
+    return dateutil.parser.parse(d).date()
+  except dateutil.parser.ParserError:
+    return None
+
+
 @app.route("/lineup", methods=['POST'])
 @slack_sig_auth
 def lineup():
+    ts = request.form['text'].split()
+    maybe_date =
+    if not len(ts):
+      return(show())
+    if len(ts) == 1:
+      try:
+        return(show((ts[0])))
+      except dateutil.parser.ParserError:
+        return 'Expected date, like "/lineup tomorrow"'
     db.collection(u'test').document(u'x').set({
         u'test': request.form['text']
     })
